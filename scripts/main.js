d0175f5f686731dca19ba5b9fe067cacd2cbf72a
Alexandre Disdier <alexandredisdier@Alexandres-MacBook-Pro.local>
2018-11-05 00:30:14 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/* main.js */
+
+/* Palindrome Checker */
+
+/* This is part of the JavaScript Algorithms and Data Structures Projects from FreeCodeCamp */
+// This is my solution for now.
+function palindrome(str) {
+  str = str.replace(/[\W_]/g, '');
+  str = str.toLowerCase();
+  var j = 1;
+  var len = str.length;
+  var count = 0;
+  var resultTxt;
+
+  for (var i = 0; i < len; i++) {
+    if (str[i] == str[len - j]) {
+      j++;
+      count++;
+    }
+  }
+
+  if (len == count) {
+    resultTxt = "This is a palindrome word";
+  } else {
+    resultTxt = "Sorry, this is not a palindrome word";
+  }
+
+  return document.getElementById("result").innerHTML = resultTxt;
+} // palindrome("eye");
+// returns True.
+// palindrome("e y e");
+// returns True.
+// palindrome("eye-ball");
+// returns false.
